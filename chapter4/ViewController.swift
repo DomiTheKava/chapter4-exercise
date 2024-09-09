b0f7b43d21d2676913e71b23a42071ca527b58ca
DomiTheKava <Dominikpenkava@yahoo.com>
2024-09-09 13:51:39 -0500

    V1

--- chapter4/ViewController.swift
+++ chapter4/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
+// MARK: the start of something
+
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var lblResult: UILabel!
+//    @IBOutlet weak var lblMessage: UILabel! // Broke connection with this
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: Methods or Functions
+    @IBAction func changeButton(_ sender: UIButton) {
+   
+    }
+    
 }
 
