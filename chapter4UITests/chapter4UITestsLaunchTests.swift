b4b2cb9cca8b328e60db02ea38631acc50e6a1fd
DomiTheKava <Dominikpenkava@yahoo.com>
2024-09-09 13:33:59 -0500

    Initial Commit

---
+++ chapter4UITests/chapter4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  chapter4UITestsLaunchTests.swift
+//  chapter4UITests
+//
+//  Created by Dominik Penkava on 9/9/24.
+//
+
+import XCTest
+
+final class chapter4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
